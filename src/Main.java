576afe0ee38740355de7ad38beee601af3a21ff1
oleg- <o.boys@yandex.ru>
2022-08-23 12:42:08 +0300

    Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11)
+        {
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println("");
+        for(int d = 10;d>=1;d--){
+            System.out.print(d+" ");
+        }
+    }
 }
